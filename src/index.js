190c01223929ddbdb217252a710f89ed7e4688af
Sanzhar Rakhimov <a777aaa@mail.ru>
2024-08-19 20:44:52 +0500

    Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    let res = Math.abs(n).toString();  
+    let result = '';
+    
+    for (let i = res.length - 1; i >= 0; i--) {
+      result += res[i];  
+    }
+    
+    return Number(result);  
+  }
   
-}
